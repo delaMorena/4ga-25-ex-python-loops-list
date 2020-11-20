7d9b54b335dcd79ce466eeb441f3cbd23ebfb2c9
delaMorena <martadmdo@gmail.com>
2020-11-20 10:09:36 +0000

    hasta el 5

--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -3,5 +3,9 @@ my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 
 #your code go here:
-
+for i in my_list:
+    hello = []
+    if type(i) == dict or (type(i) == list):
+        i_copy = i.copy()
+        hello.append(i_copy)
 print(hello)
